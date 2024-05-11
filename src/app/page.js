3578701a9993445a8fe2e2a5884e8515fed3e524
Yogesh Kumar <globalaudios736@gmail.com>
2024-05-11 22:09:40 +0530

    get the dynamic data from api and create the card list component with style

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,23 @@
 "use client";
-import { useState } from "react";
+import { CardList } from "@/components/card-list.componet";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [monsters, setMonsters] = useState([
-    {
-      name: "Pichani",
-      id: "mons1",
-    },
-    {
-      name: "Chudail",
-      id: "mons2",
-    },
-    {
-      name: "Dakani",
-      id: "mons3",
-    },
-    {
-      name: "Bharmrakshash",
-      id: "mons4",
-    },
-    {
-      name: "Jin",
-      id: "mons5",
-    },
-    {
-      name: "Bhoot",
-      id: "mons6",
-    },
-  ]);
-  console.log(monsters);
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
   return (
     <>
-      <h1>Hi, Yogesh</h1>
-      {monsters.map((monster) => (
-        <h1 key={monster.id}>{monster.name}</h1>
-      ))}
+      <CardList>
+        {monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </CardList>
     </>
   );
 }
